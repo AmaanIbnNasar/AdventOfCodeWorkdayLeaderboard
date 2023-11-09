69591c47586d33c8edbfcc94a46eb17fe39189a7
Jack Spagnoli <jack.spagnoli1@nhs.net>
2023-11-09 11:25:14 +0000

    adds model and tests

---
+++ packages/backend/models/mod.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Response {
+    event: String,
+    owner_id: isize,
+    members: Members
+}
+
+pub type Members = HashMap<String, Member>;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Member {
+    global_score: isize,
+	stars: isize,
+	id: isize,
+	name: String,
+	local_score: isize,
+	last_star_ts: isize,
+	completion_day_level: CompletionDayLevel
+}
+
+pub type CompletionDayLevel = HashMap<String, Day>;
+
+pub type Day = HashMap<String, Completion>;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Completion {
+    star_index: isize,
+    get_star_ts: isize,
+}
+
+#[cfg(test)]
+mod test;
